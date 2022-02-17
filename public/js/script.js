070ee6ef652885bb31354486ea27e4a683ed5c7d
Igor Bezanovic <i.bezanovic@levi9.com>
2022-02-17 11:24:27 +0100

    Weather app

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+console.log("Client's side");
+
+const weatherForm = document.querySelector("form");
+const searchLocation = document.querySelector("input");
+const errorMessage = document.getElementById("error-message");
+const messageText = document.getElementById("message")
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let location = searchLocation.value;
+  errorMessage.textContent = '';
+  
+  if (!location) return errorMessage.textContent = 'You must enter location...';
+  messageText.textContent = "Loading..."
+  
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        messageText.textContent = '';
+        if (data.error) return errorMessage.textContent = data.error;
+
+        messageText.textContent = data.forecast
+        console.log(data);
+      });
+    }
+  );
+});
---
+++ src/app.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (request, response) => {
+  response.render("index", {
+    name: "Igor",
+    lastName: "Bezanovic",
+  });
+});
+
+app.get("/about", (request, response) => {
+  response.render("about", {
+    name: "Igor",
+    lastName: "Bezanovic",
+  });
+});
+
+app.get("/help", (request, response) => {
+  response.render("help", {
+    name: "Igor",
+    lastName: "Bezanovic",
+  });
+});
+
+app.get("/weather", (request, response) => {
+  if (!request.query.address) {
+    return response.send({
+      placeInfo: "Morate proslediti lokaciju",
+    });
+  }
+
+  geocode(request.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) return response.send({ error });
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) return response.send({ error });
+      response.send({
+        forecast: forecastData,
+        place_info: `Address is ${request.query.address}`,
+        location,
+        latitude,
+        longitude
+      });
+    });
+  });
+});
+
+app.get("/products", (request, response) => {
+  if (!request.query.search) {
+    return response.send({
+      product: "Morate proslediti search parametar",
+    });
+  }
+
+  response.send({
+    product: [],
+  });
+});
+
+app.get("/help/*", (request, response) => {
+  response.render("404", {
+    name: "Igor",
+    lastName: "Bezanovic",
+    errorMessage: "help article not found",
+  });
+});
+
+app.get("*", (request, response) => {
+  response.render("404", {
+    name: "Igor",
+    lastName: "Bezanovic",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
